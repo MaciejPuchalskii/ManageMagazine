01af4d375e0fe206cc964fbe57982e0c5a00b127
Maciej Puchalski <maciejpe34@gmail.com>
2023-03-20 20:10:01 +0100

    Add new icons to Project and create add/edit products window

---
+++ ManageMagazine/AddEditProductWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ManageMagazine
+{
+    /// <summary>
+    /// Interaction logic for AddEditCustomer.xaml
+    /// </summary>
+    public partial class AddEditProductWindow : Window
+    {
+        public AddEditProductWindow()
+        {
+            InitializeComponent();
+        }
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
