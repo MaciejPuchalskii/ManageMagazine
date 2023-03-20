d5a7c025a18427eb626b4cc3981c94954b271c54
Maciej Puchalski <maciejpe34@gmail.com>
2023-03-20 16:09:05 +0100

    Add Product and Customer class and edit Customers and Products Windows. Create for tests lists of products/customers and displays them in ListView

--- ManageMagazine/CustomersWindow.xaml.cs
+++ ManageMagazine/CustomersWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace ManageMagazine
         public CustomersWindow()
         {
             InitializeComponent();
+            UsersListView.ItemsSource = custemerList;
         }
 
 
@@ -60,6 +61,20 @@ namespace ManageMagazine
                 MessageBox.Show(ex.Message);
             }
         }
+
+        List<Customer> custemerList = new List<Customer>()
+        {
+            new Customer() { Id = 1, FirstName = "Anna", LastName = "Kowalska", PhoneNumber = "111 111 111", City = "Warszawa", Street = "Marszałkowska", HouseNumber = "1", PostalCode = "00-001" },
+            new Customer() { Id = 2, FirstName = "Jan", LastName = "Nowak", PhoneNumber = "222 222 222", City = "Kraków", Street = "Krakowska", HouseNumber = "2", PostalCode = "30-001" },
+            new Customer() { Id = 3, FirstName = "Maria", LastName = "Nowacka", PhoneNumber = "333 333 333", City = "Gdańsk", Street = "Gdańska", HouseNumber = "3", PostalCode = "80-001" },
+            new Customer() { Id = 4, FirstName = "Adam", LastName = "Kowalski", PhoneNumber = "444 444 444", City = "Poznań", Street = "Poznańska", HouseNumber = "4", PostalCode = "60-001" },
+            new Customer() { Id = 5, FirstName = "Karolina", LastName = "Kowalczyk", PhoneNumber = "555 555 555", City = "Wrocław", Street = "Wrocławska", HouseNumber = "5", PostalCode = "50-001" },
+            new Customer() { Id = 6, FirstName = "Tomasz", LastName = "Kowalski", PhoneNumber = "666 666 666", City = "Szczecin", Street = "Szczecińska", HouseNumber = "6", PostalCode = "70-001" },
+            new Customer() { Id = 7, FirstName = "Magdalena", LastName = "Nowakowska", PhoneNumber = "777 777 777", City = "Gdynia", Street = "Gdyńska", HouseNumber = "7", PostalCode = "81-001" },
+            new Customer() { Id = 8, FirstName = "Kamil", LastName = "Nowak", PhoneNumber = "888 888 888", City = "Katowice", Street = "Katowicka", HouseNumber = "8", PostalCode = "40-001" },
+            new Customer() { Id = 9, FirstName = "Natalia", LastName = "Kowalska", PhoneNumber = "999 999 999", City = "Łódź", Street = "Łódzka", HouseNumber = "9", PostalCode = "90-001" },
+            new Customer() { Id = 10, FirstName = "Piotr", LastName = "Kowalczyk", PhoneNumber = "101 101 101", City = "Gliwice", Street = "Gliwicka", HouseNumber = "10", PostalCode = "44-100" }
+        };
 
 
 
---
+++ ManageMagazine/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageMagazine
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Manufacturer { get; set; }
+        public double PurchasePrice { get; set; }
+        public double SalePrice { get; set; }
+        public int Quantity { get; set; }
+      
+    }
+}
